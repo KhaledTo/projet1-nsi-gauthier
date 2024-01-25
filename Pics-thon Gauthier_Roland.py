f01ba05ed08b7c3d5a455d869871d130e55e7032
Khaled Ben Abdallah <khaled.ben-abdallah@etu.univ-amu.fr>
2024-01-25 12:48:12 +0100

    Modifier po en tk

--- Pics-thon Gauthier_Roland.py
+++ Pics-thon Gauthier_Roland.py
@@ -1,9 +1,9 @@
 # Créé par Elèves, le 23/11/2023 en Python 3.7
 from PIL import Image, ImageTk
-import tkinter as po
+import tkinter as tk
 
 def Niv_gris():
-    global image_a_modifier
+    #global image_a_modifier
     longueur,largeur=image_a_modifier.size
     image2=Image.new('RGB',(longueur,largeur))
     for y in range(largeur):
@@ -18,14 +18,14 @@ def Niv_gris():
     image2.save("image2.png")
     image_a_modifier=Image.open("image2.png")
     img = ImageTk.PhotoImage(Image.open("image2.png"))
-    mon_label_noir = po.Label(fenetre, image=image_a_modifier)
+    mon_label_noir = tk.Label(fenetre, image=image_a_modifier)
     mon_label_noir.pack()
-    mon_label_depart.destroy()
+    image_a_modifier.destroy()
     fenetre.mainloop()
 
 
 def Inverser():
-    global image_a_modifier
+    #global image_a_modifier
     L,H = image_a_modifier.size
     image2 = Image.new('RGB', (512, 512))
     for x in range(L):
@@ -35,30 +35,30 @@ def Inverser():
     image2.save("image2.png")
     image_a_modifier=Image.open("image2.png")
     img = ImageTk.PhotoImage(Image.open("image2.png"))
-    mon_label_inver = po.Label(fenetre, image=image_a_modifier)
+    mon_label_inver = tk.Label(fenetre, image=image_a_modifier)
     mon_label_inver.pack()
-    mon_label_depart.destroy()
+    image_a_modifier.destroy()
     fenetre.mainloop()
 
-fenetre = po.Tk()
+fenetre = tk.Tk()
 global image_a_modifier
 #image_a_modifier =Image.open('bebe-yoda.jpg')
 fenetre.title("Pics-thon Gauthier_Roland")
 fenetre.geometry("1080x700")
 fenetre.minsize(1000, 800)
 fenetre.config(background='#FF6133')
-fenetre.iconbitmap("Pics-thon_logo.ico")
-boite = po.Frame(fenetre, bg='#000000', bd=1, relief= po.SUNKEN)
-label_title = po.Label(boite, text=" Outils d'édition", bg='#000000', fg='white', font=("Courier, 25"))
+fenetre.iconphoto(False, tk.PhotoImage(file="Pics-thon_logo.ico"))
+boite = tk.Frame(fenetre, bg='#000000', bd=1, relief= tk.SUNKEN)
+label_title = tk.Label(boite, text=" Outils d'édition", bg='#000000', fg='white', font=("Courier, 25"))
 image_a_modifier = Image.open('bebe-yoda.jpg')
 
 
-#image_a_modifier = po.Label(fenetre, image=image_a_modifier)
+#image_a_modifier = tk.Label(fenetre, image=image_a_modifier)
 #image_a_modifier.pack()
-b = po.Button(boite , text= "Tourner l'image", bg='#FF6133', fg='white', font=("Courier, 20"))
-b1 = po.Button(boite , text= "Niveaux de gris", bg='#FF6133', fg='white', font=("Courier, 20"), command=Niv_gris)
-b2 = po.Button(boite , text= "Eclaircir", bg='#FF6133', fg='white', font=("Courier, 20"))
-b3 = po.Button(boite , text= "Inverser l'image", bg='#FF6133', fg='white', font=("Courier, 20"), command=Inverser)
+b = tk.Button(boite , text= "Tourner l'image", bg='#FF6133', fg='white', font=("Courier, 20"))
+b1 = tk.Button(boite , text= "Niveaux de gris", bg='#FF6133', fg='white', font=("Courier, 20"), command=Niv_gris)
+b2 = tk.Button(boite , text= "Eclaircir", bg='#FF6133', fg='white', font=("Courier, 20"))
+b3 = tk.Button(boite , text= "Inverser l'image", bg='#FF6133', fg='white', font=("Courier, 20"), command=Inverser)
 
 boite.pack(anchor='ne')
 
@@ -68,7 +68,7 @@ b.pack()
 b1.pack()
 b2.pack()
 b3.pack()
-panel = po.Label(fenetre, image = image_a_modifier)
+panel = tk.Label(fenetre, image = image_a_modifier)
 panel.pack()
 
 fenetre.mainloop()
